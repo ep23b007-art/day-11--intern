35236f0e14c66f6c6f38c531f0eeec918e143b97
Sanjana Chowdary <ep23b007@smail.iitm.ac.in>
2026-07-10 00:18:38 +0530

    day12: prompt v1.1 format lock + orchestrator fixes + live widget

--- app/core/config.py
+++ app/core/config.py
@@ -3,5 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = "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"
-GROQ_MODEL = "llama-3.3-70b-versatile"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
